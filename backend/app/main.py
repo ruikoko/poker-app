7288d77650545e1d2d83336cbccd968a50c8057b
ruikoko <ruidias@ilhavo.org>
2026-03-25 12:16:03 +0000

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,6 @@ import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
-
 from app.routers import health, auth, import_, tournaments, hands, villains
 
 load_dotenv()
